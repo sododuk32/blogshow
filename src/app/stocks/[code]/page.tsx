f4f545288cf53c4b7a28a5b84ae1d1209126fce7
dy k <37325163+sododuk32@users.noreply.github.com>
2025-04-03 16:30:35 +0900

    sadf

--- src/app/stocks/[code]/page.tsx
+++ src/app/stocks/[code]/page.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+// import React from 'react';
 
-async function getData(code: string | undefined) {
-  // 서버에서 호출해야 하는 로직을 여기서 직접 처리
-  return code ?? 'noData';
+// async function getData(code: string | undefined) {
+//   // 서버에서 호출해야 하는 로직을 여기서 직접 처리
+//   return code ?? 'noData';
+// }
+
+// type Props = {
+//   searchParams: { code?: string };
+// };
+
+// export default async function StocksPage({ searchParams }: Props) {
+//   const data = await getData(searchParams.code);
+
+//   return <div>{data}</div>;
+// }
+// app/stocks/[code]/page.tsx
+
+export async function generateStaticParams() {
+  return [{ code: 'apple' }, { code: 'samsung' }, { code: 'tesla' }];
 }
 
 type Props = {
-  searchParams: { code?: string };
+  params: { code: string };
 };
 
-export default async function StocksPage({ searchParams }: Props) {
-  const data = await getData(searchParams.code);
-
-  return <div>{data}</div>;
+export default function StockDetailPage({ params }: Props) {
+  return <div>상세 페이지: {params.code}</div>;
 }
