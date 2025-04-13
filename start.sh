94ca65b05c255be971703ce7e0ada8cbe4669ab0
sododuk32 <sododuk63@gmail.com>
2025-04-13 14:20:10 +0900

    no key checking sh

--- start.sh
+++ start.sh
@@ -2,27 +2,27 @@ echo "Starting Next.js with PM2..."
 pm2 start npm --name "nextjs" -- run start
 #!/bin/bash
 
-TRY_COUNT=0
-MAX_TRIES=60
+# TRY_COUNT=0
+# MAX_TRIES=60
 
-while true; do
-  RESPONSE=$(curl -s http://localhost:3000/api/giveKey)
-  MESSAGE=$(echo "$RESPONSE" | jq -r .message)
+# while true; do
+#   RESPONSE=$(curl -s http://localhost:3000/api/giveKey)
+#   MESSAGE=$(echo "$RESPONSE" | jq -r .message)
 
-  if [ "${#MESSAGE}" -ge 4 ]; then
-    echo "✅ message 길이: ${#MESSAGE}, 내용: $MESSAGE"
-    break
-  fi
+#   if [ "${#MESSAGE}" -ge 4 ]; then
+#     echo "✅ message 길이: ${#MESSAGE}, 내용: $MESSAGE"
+#     break
+#   fi
 
-  echo "Waiting... 현재 message 길이: ${#MESSAGE}, 내용: $MESSAGE"
-  sleep 1
-  TRY_COUNT=$((TRY_COUNT+1))
+#   echo "Waiting... 현재 message 길이: ${#MESSAGE}, 내용: $MESSAGE"
+#   sleep 1
+#   TRY_COUNT=$((TRY_COUNT+1))
 
-  if [ "$TRY_COUNT" -ge "$MAX_TRIES" ]; then
-    echo "❌ Timeout waiting for valid message. Exiting."
-    exit 1
-  fi
-done
+#   if [ "$TRY_COUNT" -ge "$MAX_TRIES" ]; then
+#     echo "❌ Timeout waiting for valid message. Exiting."
+#     exit 1
+#   fi
+# done
 
 
 
