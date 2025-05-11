0e6c5684f24b684b9d6625e4cdef17d6a646825a
sododuk32 <sododuk63@gmail.com>
2025-05-11 17:55:44 +0900

    clean worker

--- public/shared-worker.js
+++ public/shared-worker.js
@@ -1,22 +1,22 @@
 // Must be in ESM form!
-console.log('[Worker] Loaded');
+console.log('Worker! Loaded');
 
 const ports = [];
 
 self.addEventListener('connect', (event) => {
-  console.log('[Worker] connect');
+  console.log('Worker! connect');
 
   const port = event.ports[0];
   ports.push(port);
 
   port.onmessage = (e) => {
-    console.log('[Worker] got message:');
+    console.log('Worker! got message:');
     console.log(e.data)
-    port.postMessage('[echo] ' )
+    port.postMessage('echo! ' )
     port.postMessage(e.data);
   };
 
   port.start();
 });
 
-export {}; // ✅ absolutely required for ESM parsing
+export {};
